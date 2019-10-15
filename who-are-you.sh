b85ce7074ad3aaaf0cb669f39fe82f5c76fc0e2c
mukkhit <mukkhit@icloud.com>
2019-10-15 19:07:26 +0600

    4

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/mukkhit/superhero-api/master/api/all.json | jq -M 'map(select(.id == 70)) | .[].name'
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -M 'map(select(.id == 70)) | .[].name'
