c99a088ff1432c53746fe5299e37f1928abeae4f
mukkhit <mukkhit@icloud.com>
2019-10-17 20:10:24 +0600

    osdf

--- printcomb2.go
+++ printcomb2.go
@@ -5,23 +5,22 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for k := '0'; k <= '9'; k++ {
-			for l :='0'; l <='9'; l++ {
-				for j :='0'; j <='9'; j++ {
-				 z01.PrintRune(i)
-			     z01.PrintRune(k)
-			     z01.PrintRune(' ')
-			     z01.PrintRune(l)
-			     z01.PrintRune(j)
-                 if i !='9' || k !='9' || l !='8' || j !='9' {
-		
-			     
-			     z01.PrintRune(',')
-			     z01.PrintRune(' ')
-			 }
+			for l := '0'; l <= '9'; l++ {
+				for j := '0'; j <= '9'; j++ {
+					z01.PrintRune(i)
+					z01.PrintRune(k)
+					z01.PrintRune(' ')
+					z01.PrintRune(l)
+					z01.PrintRune(j)
+					if i != '9' || k != '9' || l != '8' || j != '9' {
 
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+
+				}
+			}
+		}
 	}
-}
-}
-}
-z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
