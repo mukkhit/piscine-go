8f6ded8cb022756d298494bbfa1dd4bc55d5ad3b
mukkhit <mukkhit@icloud.com>
2019-10-17 19:09:43 +0600

    oeg

--- printcomb.go
+++ printcomb.go
@@ -4,19 +4,19 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
-		    for l := i + 1; l <= '9'; l++ {
-		    	for k := l + 1; k <= '9'; k++ {
-		    		z01.PrintRune(i)
-		    		z01.PrintRune(l)
-		    		z01.PrintRune(k)
-		    		if i != '7' || l != '8' || k != '9' {
-		    			z01.PrintRune(',')
-		    			z01.PrintRune(' ')
+		for l := i + 1; l <= '9'; l++ {
+			for k := l + 1; k <= '9'; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(l)
+				z01.PrintRune(k)
+				if i != '7' || l != '8' || k != '9' {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 
-		    	}
-		    		}
+				}
+			}
 
-		    } 
+		}
 	}
 	z01.PrintRune('\n')
 }
