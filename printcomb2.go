5d025679baaf70f7f6adc002f018afe22eb534f6
mukkhit <mukkhit@icloud.com>
2019-10-17 20:33:47 +0600

    to git

--- printcomb2.go
+++ printcomb2.go
@@ -8,21 +8,21 @@ func PrintComb2() {
 			for l := '0'; l <= '9'; l++ {
 				for j := '0'; j <= '9'; j++ {
 					if i == l && k < l || i < l {
-					z01.PrintRune(i)
-					z01.PrintRune(k)
-					z01.PrintRune(' ')
-					z01.PrintRune(l)
-					z01.PrintRune(j)
-					if i != '9' || k != '8' || l != '9' || j != '9' {
-
-						z01.PrintRune(',')
+						z01.PrintRune(i)
+						z01.PrintRune(k)
 						z01.PrintRune(' ')
-					}
+						z01.PrintRune(l)
+						z01.PrintRune(j)
+						if i != '9' || k != '8' || l != '9' || j != '9' {
 
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+
+					}
 				}
 			}
 		}
 	}
-}
 	z01.PrintRune('\n')
 }
