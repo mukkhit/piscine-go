2e356ed03830855925220f384a04e85b23e10ccd
mukkhit <mukkhit@icloud.com>
2019-10-21 18:04:14 +0600

    to git

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -2,13 +2,13 @@ package piscine
 
 func RecursiveFactorial(nb int) int {
 	if nb >= 0 && nb < 2147483647 {
-	if nb == 1 {
-		return 1
+		if nb == 1 {
+			return 1
+		}
+		if nb > 1 {
+			return nb * RecursiveFactorial(nb-1)
+		}
+		return 0
 	}
-       if nb > 1 {
-       	return nb * RecursiveFactorial(nb - 1)
-       }
 	return 0
-} 
-return 0
 }
