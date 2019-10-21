19288e20b6f8f8c171772475e95bd7e69991c0e3
mukkhit <mukkhit@icloud.com>
2019-10-21 17:36:04 +0600

    to git

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,7 +2,7 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	arg := 1
-	if nb > 0 && nb < 2147483647 {
+	if nb >= 0 && nb < 2147483647 {
 		for i := 1; i < nb+1; i++ {
 			arg = arg * i
 		}
