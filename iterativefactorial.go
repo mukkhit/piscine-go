df28a91b20405a163a0b1258a69702a64e228cad
mukkhit <mukkhit@icloud.com>
2019-10-21 17:27:26 +0600

    to git

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,11 +2,10 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	arg := 1
-	if nb >= 0 && nb < 2147483647 {
+	if nb > 0 && nb < 20 {
 		for i := 1; i < nb+1; i++ {
 			arg = arg * i
 		}
 	}
 	return arg
-
 }
